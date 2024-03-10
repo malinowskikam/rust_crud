7a57d5529d8f72e7807baef7bfdd009f5d2dd3bd
Kamil Malinowski <malinowskikam96@gmail.com>
2024-03-10 15:02:25 +0100

    Hyper hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use std::convert::Infallible;
+use std::net::SocketAddr;
+
+use http_body_util::Full;
+use hyper::body::Bytes;
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::{Request, Response};
+use hyper_util::rt::TokioIo;
+use tokio::net::TcpListener;
+
+async fn hello(req: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+    println!("[{}] {}", req.method(), req.uri());
+    Ok(Response::new("Hello, world!".into()))
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
+    let listener = TcpListener::bind(&addr).await?;
+    println!("Listening on http://{}", addr);
+    loop {
+        let (stream, _) = listener.accept().await?;
+
+        let io = TokioIo::new(stream);
+        tokio::spawn(async move {
+            if let Err(e) = http1::Builder::new()
+                .serve_connection(io, service_fn(hello))
+                .await
+            {
+                eprintln!("[Error]: {}", e);
+            }
+        });
+    }
 }
